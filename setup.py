a6d52918997f567b5eb786d8d9f6d169763d22f6
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2017-09-06 11:10:26 +0200

    Working test

--- setup.py
+++ setup.py
@@ -23,7 +23,15 @@ VERSION = "1.0.0"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "fire"]
+REQUIRES = [
+        "urllib3 >= 1.15", 
+        "six >= 1.10", 
+        "certifi", 
+        "python-dateutil", 
+        "fire", 
+        "requests",
+        "requests_kerberos"
+        ]
 
 setup(
     name=NAME,
